66dff12bdc426ac6499d9387386650d81b2ff7c0
Paulo Takaiama <ptakaiama@gmail.com>
2026-09-24 13:43:34 -0300

    v5.5.0 - historico de versoes e log de atividades

--- netlify/functions/api.js
+++
@@ -1,9 +0,0 @@
-const serverless = require('serverless-http');
-const { app, initDb } = require('../../app');
-
-const expressHandler = serverless(app);
-
-exports.handler = async (event, context) => {
-  await initDb();
-  return expressHandler(event, context);
-};
